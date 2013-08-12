3a6d11108d7cbec5cb81554acf492505b823b749
carymrobbins <carymrobbins@gmail.com>
2013-08-11 19:56:44 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Metis::Application.config.secret_key_base = '5abe1e0e375dff54014696e435807cb1c1e9898e22139c65607ebdc75854573fa08f01ebf5995581b0a338b5e61065da1d56786878faf97a09d3d76bfae6481e'
