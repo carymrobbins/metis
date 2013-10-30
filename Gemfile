84176da2fc65e7c156ade52a759bac8d4b7e7295
Cary Robbins <carymrobbins@gmail.com>
2013-10-29 22:04:41 -0500

    Updated style for list page.

--- Gemfile
+++ Gemfile
@@ -40,6 +40,9 @@ end
 # Use ActiveModel has_secure_password
 gem 'bcrypt-ruby', '3.0.1'
 
+# Use Intuit API
+gem 'aggcat'
+
 # Use unicorn as the app server
 # gem 'unicorn'
 
--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -4,4 +4,29 @@ class ListsController < ApplicationController
   def index
     @lists = current_user.lists
   end
+
+  def new
+    @list = current_user.lists.build
+  end
+
+  def create
+    @list = current_user.lists.build(list_params)
+    if @list.save
+      flash[:success] = 'List created!'
+      redirect_to lists_path
+    else
+      render 'new'
+    end
+  end
+
+  def show
+    @list = current_user.lists.find(params[:id])
+  end
+
+  private
+
+    def list_params
+      params.require(:list).permit(:name)
+    end
+
 end
